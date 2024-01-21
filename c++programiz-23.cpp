a9b0ebbf0855263d31a772f182d8a950433b532b
Cheris Quessou <quessoucheris144@gmail.com>
2024-01-21 12:09:32 +0000

    feat: implemented c++ programiz 23 and 24

---
+++ c++programiz-23.cpp
@@ -0,0 +1,17 @@
+// C++ Program to Multiply two Numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  double num1, num2, product;
+  cout << "Enter two numbers: ";
+
+  cin >> num1 >> num2;
+ 
+  product = num1 * num2;  
+
+  cout << "Product = " << product;    
+    
+  return 0;
+}
